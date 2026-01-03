cafa9c6799f9bb764b4563b2be13cf2257d95e3a
Edward Cafarella <ed_Caf@protonmail.com>
2026-01-03 12:55:09 -0500

    Removed unused Lessons

--- src/components/lessons/coldcard/index.js
+++
@@ -1,4 +0,0 @@
-export { DeviceOverview } from './DeviceOverview';
-export { SecurityLayers } from './SecurityLayers';
-export { PSBTWorkflow } from './PSBTWorkflow';
-export { AdvancedFeatures } from './AdvancedFeatures';
--- src/components/lessons/sparrow/index.js
+++
@@ -1,4 +0,0 @@
-export { WalletSetup } from './WalletSetup';
-export { MultisigSetup } from './MultisigSetup';
-export { TransactionWorkflow } from './TransactionWorkflow';
-export { NodeConnection } from './NodeConnection';
